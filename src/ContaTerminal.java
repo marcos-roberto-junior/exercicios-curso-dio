7771e6f5eec3ac0055e03b1838f6bbb17c58f756
marco <marcosroj@escoladotrabalhador40.com.br>
2023-08-25 22:49:07 -0300

    Projeto conta bancaria DIO

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+        Scanner imput = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o seu nome! ");
+        nomeCliente = imput.next();
+        System.out.println("Por favor, digite o numero da conta! ");
+        numero = imput.nextInt();
+        System.out.println("Por favor, digite o numero da agencia! ");
+        agencia = imput.next();
+        System.out.println("Por favor, digite o saldo inicial! ");
+        saldo = imput.nextDouble();
+
+        System.out.println("Ola "+nomeCliente+", " +
+                "obrigado por criar uma conta em nosso banco, " +
+                "sua agencia é "+agencia+", conta "+numero+
+                " e seu saldo "+saldo+" já esta disponivel.");
     }
 }
